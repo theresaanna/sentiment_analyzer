11146289ddeec023fa37b673fc4ea97abccf0638
theresa <theresasumma@gmail.com>
2025-09-21 10:49:41 -0700

    fix tests

--- app/__init__.py
+++ app/__init__.py
@@ -70,7 +70,7 @@ def create_app(config_class=Config):
     app.register_blueprint(main_bp)
     
     from app.auth import bp as auth_bp
-    app.register_blueprint(auth_bp)
+    app.register_blueprint(auth_bp, url_prefix='/auth')
     
     from app.main.channel_routes import bp as channel_bp
     app.register_blueprint(channel_bp)
